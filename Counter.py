47d512d223c85d9011e51f8631da5ae50bd4715b
Samuel Hu <hus1@bxscience.edu>
2022-04-20 23:59:45 -0400

    fixed Japanese datasets

--- Counter.py
+++ Counter.py
@@ -6,6 +6,9 @@ unique_words = set()
 total_lines = 0
 num_syll = 0
 num_words = 0
+num_syll_in_words = 0
+list = []
+total_num_of_syll_in_unique_words = 0
 
 my_dir = os.getcwd()
 pNameG = "Japanese/Okayama/Okayama_train_gold.txt"
@@ -26,5 +29,17 @@ with open(os.path.join(my_dir,pNameG)) as data:
         unique_words = unique_words.union(words_in_line)
     #print(unique_words)
 
+for e in unique_words:
+    num_syll_in_words += e.count('.')+1
+    list.append(e.count('.')+1)
 
-print("Unique syllables: ", len(unique_syll), "\nNum syllables: ",num_syll, "\nUnique words: ",len(unique_words), "\nNum words: ",num_words)
+for num in list:
+    total_num_of_syll_in_unique_words += num
+#print(len(list), num_syll_in_words)
+
+for a,b in zip(list, unique_words):
+    if(a == 1):
+        print(b,a)
+
+
+# print("Total lines: ",total_lines,"\nUnique syllables: ", len(unique_syll), "\nNum syllables: ",num_syll, "\nUnique words: ",len(unique_words), "\nNum words: ",num_words,"\n",)
